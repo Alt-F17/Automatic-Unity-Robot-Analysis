05b30c28924eb5e2c8a0211d46b40920971aa330
Felix Egan <felix.egan+git@icloud.com>
2026-04-14 08:55:59 -0400

    fix: validate sceneName before loading scene in StartButton

--- Assets/Scripts/Interactivity/StartButton.cs
+++ Assets/Scripts/Interactivity/StartButton.cs
@@ -1,14 +1,23 @@
 using UnityEngine;
-using Unity.SceneManagement;
+using UnityEngine.SceneManagement;
 
 
 
 public class StartButton : MonoBehaviour
 {
+    [SerializeField] private string sceneName = "MainScene";
+
     public void StartSimulation()
     {
         Debug.Log("Start Button Clicked");
-        SceneManager.LoadScene("MainScene");
+
+        if (string.IsNullOrWhiteSpace(sceneName))
+        {
+            Debug.LogError("StartButton: sceneName is empty.");
+            return;
+        }
+
+        SceneManager.LoadScene(sceneName);
     }
 
     public void QuitGame(){
